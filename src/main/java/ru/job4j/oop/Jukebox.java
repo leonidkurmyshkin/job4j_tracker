9900ebcc042e0fc99fc54089d9778d772c91cae2
leonidkurmyshkin <leonid2073@gmail.com>
2021-02-04 18:00:10 +0300

    1.3. Вызов метода с аргументами. [#446954]

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+
+    public void music(int position) {
+        String lyrics = switch (position) {
+            case 1 -> "Пусть бегут неуклюже \n"
+                    + "Пешеходы по лужам, \n"
+                    + "А вода по асфальту рекой. \n"
+                    + "И не ясно прохожим \n"
+                    + "В этот день непогожий \n"
+                    + "Почему я весёлый такой.";
+            case 2 -> "Спят усталые игрушки, книжки спят.\n"
+                    + "Одеяла и подушки ждут ребят.\n"
+                    + "Даже сказка спать ложится,\n"
+                    + "Чтобы ночью нам присниться.\n"
+                    + "Ты ей пожелай:\n"
+                    + "Баю-бай.";
+            default -> "Песня не найдена.";
+        };
+        System.out.println(lyrics);
+    }
+
+    public static void main(String[] args) {
+        Jukebox lyrics = new Jukebox();
+        System.out.println("Текст песни 1:");
+        lyrics.music(1);
+        System.out.println("\nТекст песни 2:");
+        lyrics.music(2);
+        System.out.println("\nТекст песни 5:");
+        lyrics.music(5);
+    }
+}
