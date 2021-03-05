cbf252ce9154cc364b6d2c14abb410d6bb50aa3d
leonidkurmyshkin <leonid2073@gmail.com>
2021-03-05 20:59:46 +0300

    Упражнения. 5. Изменение уже существующей записи в отображении.

---
+++ src/main/java/ru/job4j/map/ComputeIfAbsent.java
@@ -0,0 +1,53 @@
+package ru.job4j.map;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class ComputeIfAbsent {
+    public static Map<Integer, String> collectData(Map<Integer, String> names, List<User> users) {
+        for (User user : users) {
+            if (names.containsKey(user.id)) {
+                names.computeIfPresent(user.id, (index, value) -> user.name);
+            } else {
+                names.computeIfAbsent(user.id, value -> user.name);
+            }
+        }
+        return names;
+    }
+
+    public static class User {
+        private int id;
+        private String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            ComputeIfAbsent.User user = (ComputeIfAbsent.User) o;
+            return Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+}
