08ca8a34a7b0a362a1b6cb5bc05b52100da42ec2
leonidkurmyshkin <leonid2073@gmail.com>
2021-03-12 16:52:34 +0300

    0. Checkstyle с Maven [#446865]

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -7,11 +7,14 @@ public class MagicBall {
     public static void main(String[] args) {
         System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
         new Scanner(System.in).nextLine();
-        String answer = switch (new Random().nextInt(3)) {
-            case 0 -> "Да";
-            case 1 -> "Нет";
-            default -> "Может быть";
-        };
+        String answer;
+        switch (new Random().nextInt(3)) {
+            case 0 : answer = "Да";
+                break;
+            case 1 : answer = "Нет";
+                break;
+            default : answer = "Может быть";
+        }
         System.out.println(answer);
     }
 }
