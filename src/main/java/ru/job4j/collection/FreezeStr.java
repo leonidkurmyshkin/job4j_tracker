a85de0645d82455fdcc534963fbbcc1d57736fb1
leonidkurmyshkin <leonid2073@gmail.com>
2021-06-11 10:32:54 +0300

    1. Экзамен. Коллекции. [#10097 #166231]. Переделал на compute

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -6,21 +6,14 @@ public class FreezeStr {
     public static boolean eq(String left, String right) {
         Map<Character, Integer> symbolsMap = new HashMap<>();
         for (var i = 0; i < left.length(); i++) {
-            var symbol = left.charAt(i);
-            var amount = symbolsMap.putIfAbsent(symbol, 1);
-            if (amount != null) {
-                symbolsMap.replace(symbol, amount + 1);
-            }
+            symbolsMap.compute(left.charAt(i),
+                    (symbol, amount) -> amount == null ? 1 : amount + 1);
         }
         var rsl = true;
-        for (var i = 0; i < right.length(); i++) {
-            var symbol = right.charAt(i);
-            var amount = symbolsMap.get(symbol);
-            if (amount == null || amount == 0) {
-                rsl = false;
-                break;
-            }
-            symbolsMap.replace(symbol, amount - 1);
+        for (var i = 0; rsl && i < right.length(); i++) {
+            rsl = symbolsMap.compute(right.charAt(i),
+                    (symbol, amount) -> amount == null ? -1 : amount - 1)
+                    >= 0;
         }
         return rsl;
     }
