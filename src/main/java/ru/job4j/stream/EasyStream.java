da7726eaedb3b1bf3080aba1e21e6c74d892a1f4
leonidkurmyshkin <leonid2073@gmail.com>
2021-06-28 11:00:50 +0300

    1. Экзамен. Lambda, Stream API, JDK Features. [#86058 #170328]. Переделал как надо

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -6,124 +6,53 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EasyStream {
-    private EasyStream firstStream;
-    private EasyStream nextStream;
-    private Method method;
     private List<Integer> source;
-    private Function<Integer, Integer> mapFunction;
-    private Predicate<Integer> filterPredicate;
-
-    enum Method {
-        OF, MAP, FILTER, COLLECT
-    }
 
     private EasyStream() {
     }
 
     public static EasyStream of(List<Integer> source) {
-        var firstStream = new Builder()
+        return new Builder()
                 .buildSource(source)
                 .build();
-        firstStream.firstStream = firstStream;
-        return firstStream;
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
-        nextStream = new Builder()
-                .buildMap(fun)
-                .buildFirstStream(firstStream)
+        List<Integer> rslList = new ArrayList<>();
+        source.forEach(number -> rslList.add(
+                fun.apply(number)));
+        return new Builder()
+                .buildSource(rslList)
                 .build();
-        return nextStream;
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
-        nextStream = new Builder()
-                .buildFilter(fun)
-                .buildFirstStream(firstStream)
+        List<Integer> rslList = new ArrayList<>();
+        source.forEach(number -> {
+                if (fun.test(number)) {
+                    rslList.add(number);
+                }
+        });
+        return new Builder()
+                .buildSource(rslList)
                 .build();
-        return nextStream;
     }
 
     public List<Integer> collect() {
-        nextStream =  new Builder()
-                .buildCollect()
-                .buildFirstStream(firstStream)
-                .build();
-        return firstStream.processStream();
-    }
-
-    private List<Integer> processStream() {
-        List<Integer> rslList = new ArrayList<>();
-        for (var rsl : source) {
-            boolean isNotSkip = true;
-            for (var chainStream = firstStream;
-                    chainStream != null && isNotSkip;
-                    chainStream = chainStream.nextStream
-            ) {
-                switch (chainStream.method) {
-                    case MAP:
-                        rsl = chainStream.mapFunction
-                                .apply(rsl);
-                        break;
-                    case FILTER:
-                        if (chainStream.filterPredicate
-                                .negate()
-                                .test(rsl)) {
-                            isNotSkip = false;
-                        }
-                        break;
-                    case COLLECT:
-                        rslList.add(rsl);
-                        break;
-                    default:
-                }
-            }
-        }
-        return rslList;
+        return source;
     }
 
     static class Builder {
-        private EasyStream firstStream;
-        private Method method;
         private List<Integer> source;
-        private Function<Integer, Integer> mapFunction;
-        private Predicate<Integer> filterPredicate;
-
-        Builder buildFirstStream(EasyStream firstStream) {
-            this.firstStream = firstStream;
-            return this;
-        }
 
         Builder buildSource(List<Integer> source) {
-            method = Method.OF;
             this.source = source;
-            return this;
-        }
-
-        Builder buildMap(Function<Integer, Integer> fun) {
-            method = Method.MAP;
-            mapFunction = fun;
-            return this;
-        }
-
-        Builder buildFilter(Predicate<Integer> fun) {
-            method = Method.FILTER;
-            filterPredicate = fun;
-            return this;
-        }
-
-        Builder buildCollect() {
-            method = Method.COLLECT;
             return this;
         }
 
         EasyStream build() {
             var easyStream = new EasyStream();
-            easyStream.firstStream = firstStream;
-            easyStream.method = method;
             easyStream.source = source;
-            easyStream.mapFunction = mapFunction;
-            easyStream.filterPredicate = filterPredicate;
             return easyStream;
         }
     }
